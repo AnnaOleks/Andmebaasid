1808ccabc3a6b32a1ea376c248a47f613836369d
Anna Oleks <annaoleks88@gmail.com>
2025-01-30 13:41:20 +0200

    Update SQL_server_film.sql

--- SQL_server_film.sql
+++ SQL_server_film.sql
@@ -39,3 +39,53 @@ FOREIGN KEY(zanrID) REFERENCES zanr(zanrID);
 select *from film;
 select *from zanr;
 UPDATE film SET zanrID=1 where filmID=3
+
+CREATE TABLE rezisoor(
+rezID int PRIMARY KEY identity(1,1),
+rezNimi varchar(20),
+synniaeg date,
+riik varchar(20)
+)
+
+ALTER TABLE film ADD rezID int;
+SELECT * FROM film;
+
+INSERT INTO rezisoor(rezNimi,synniaeg,riik)
+VALUES('Quentin Tarantino', '1963-03-27', 'USA');
+SELECT * FROM rezisoor;
+
+select *from film;
+select *from zanr;
+select *from rezisoor;
+
+ALTER TABLE film ADD CONSTRAINT fk_rezisoor
+FOREIGN KEY(rezID) REFERENCES rezisoor(rezID);
+
+UPDATE film SET rezID=2 where filmID=6
+UPDATE film SET zanrID=2 where filmID=2
+
+CREATE TABLE kinokava(
+kavaID int PRIMARY KEY identity(1,1),
+filmID int,
+rezID int,
+kuupaev date,
+aeg time
+)
+DROP TABLE kinokava
+
+select *from film;
+select *from zanr;
+select *from rezisoor;
+select *from kinokava;
+
+INSERT INTO kinokava(filmID,rezID,kuupaev,aeg)
+VALUES(6, 2, '2025-01-30','19:00');
+
+ALTER TABLE film ADD CONSTRAINT fk_zanr
+FOREIGN KEY(zanrID) REFERENCES zanr(zanrID);
+
+ALTER TABLE kinokava ADD CONSTRAINT fk_film
+FOREIGN KEY(filmID) REFERENCES film(filmID);
+
+ALTER TABLE kinokava ADD CONSTRAINT fkf_rezisoor
+FOREIGN KEY(rezID) REFERENCES rezisoor(rezID);
